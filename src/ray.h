bda477c48ed4a567d44f0e44e2638fe88828f519
Creator <sergej.kosov@gmail.com>
2019-10-04 20:01:52 +0200

    Update ray.h

--- src/ray.h
+++ src/ray.h
@@ -12,5 +12,5 @@ struct Ray
 	Vec3f		org;		///< Origin
 	Vec3f		dir;		///< Direction
 	float		t;		///< Current/maximum hit distance
-	comst CPrim* 	hit;		///< Pointer to currently closest primitive
+	const CPrim* 	hit;		///< Pointer to currently closest primitive
 };
